1d7e3ebcc28e00ea584011327258c671f007c71b
andrecarepa1 <a85259@ualg.pt>
2023-11-17 12:15:39 +0000

    Alteração do temporizador

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Site André Carepa',
-  tagline: 'docusaurus-ghpages',
+  tagline: 'Viagem ao Porto',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
